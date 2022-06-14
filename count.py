2c817fc08337b62eb155f77e899376e61529469c
Mauricio Costa <malesupremacy91@hotmail.com>
2022-06-14 12:56:53 -0300

    count

---
+++ count.py
@@ -0,0 +1,30 @@
+#afirmando o que irá fazer
+print ("I will now count my chickens: ")
+# vai dar o resultado abaixo de galinhas
+print ("Hens", 25 + 30 / 6)
+#agora o resultado de galos
+print ("Roosters", 100 - 25 * 3 % 4)
+#agora irá contar os ovos
+print ("Now i will count the eggs: ")
+# faz todo o cálculo
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# faz a pergunta, e abaixo, a conta. Caso a conta seja verdadeira, aparece
+# como resultado o True, caso seja falso, aparece naturalmente o False
+print (" Is it true that 3 + 2 < 5 - 7?")
+print (3 + 2 < 5 - 7)
+
+print ("What is 3 + 2?", 3+2)
+print ("What is 5 - 7?", 5-7)
+# acima duas contas básicas
+print ("Oh, that's why it's False.")
+# explicando por que o resultado foi falso, baseado nas contas acima.
+
+print ("How about some more.")
+#abaixo, exemplos do resultado True ou False, de acordo com as contas.
+#Isso aparece naturalmente com o Python.
+print ("It is greater?", 5>-2)
+print ("Is it greater or equal?", 5>=-2)
+print ("Is it less or equal?", 5<= -2)
+
+#Esse projeto está em inglês pois estou treinando
+# ambos simultaneamente, Inglês e Python. Farei mais assim. 
